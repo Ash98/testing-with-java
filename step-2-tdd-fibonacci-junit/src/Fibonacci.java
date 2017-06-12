bf2dc15d2b06fc02aba418ca0da344455dabf145
Ash98 <ashleyaburnett@gmail.com>
2017-06-12 08:29:39 +0100

    Add step 3 files

--- step-2-tdd-fibonacci-junit/src/Fibonacci.java
+++ step-2-tdd-fibonacci-junit/src/Fibonacci.java
@@ -1,2 +1,16 @@
 public class Fibonacci {
+    public int calculate(int index) {
+
+        if(index < 0){
+            throw new IllegalArgumentException("DO NOT ENTER A NEGATIVE VALUE");
+        }
+
+        if(index == 0 || index == 1){
+            return 1;
+        }
+        else {
+            return calculate(index-1)+calculate(index-2);
+        }
+
+    }
 }
